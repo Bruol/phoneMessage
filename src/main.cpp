08adf8d9da569b8534c5d61df6243f1744a87d18
bruol <lorin@urbantat.eu>
2026-06-02 00:20:15 +0200

    add switches for phone pick up and reset

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,10 @@
 #define I2S_BCLK 26
 #define I2S_LRC 25
 
+#define RESET 21
+#define PhonePicked 4
+#define SWITCH_DEBOUNCE_MS 50
+
 #define AUDIO_FILE "/Lorin_Urbantat.wav"
 
 const byte ROWS = 4;
@@ -37,7 +41,76 @@ byte colPins[COLS] = {27, 16, 17};
 Audio audio;
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-byte activeKeys = 0;
+const byte switchPins[] = {RESET, PhonePicked};
+const byte switchCount = sizeof(switchPins) / sizeof(switchPins[0]);
+
+byte activeInputs = 0;
+bool switchClosed[switchCount] = {false, false};
+bool lastSwitchReading[switchCount] = {HIGH, HIGH};
+unsigned long lastSwitchChange[switchCount] = {0, 0};
+
+void inputClosed(const char *inputName)
+{
+  activeInputs++;
+  Serial.printf("%s closed: starting audio\n", inputName);
+  audio.stopSong();
+  audio.connecttoFS(SD, AUDIO_FILE);
+}
+
+void inputOpened(const char *inputName)
+{
+  if (activeInputs > 0)
+  {
+    activeInputs--;
+  }
+
+  Serial.printf("%s open\n", inputName);
+
+  if (activeInputs == 0)
+  {
+    Serial.println("All inputs open: stopping audio");
+    audio.stopSong();
+  }
+}
+
+void readSwitches()
+{
+  for (byte i = 0; i < switchCount; i++)
+  {
+    bool reading = digitalRead(switchPins[i]);
+
+    if (reading != lastSwitchReading[i])
+    {
+      lastSwitchReading[i] = reading;
+      lastSwitchChange[i] = millis();
+    }
+
+    if (millis() - lastSwitchChange[i] < SWITCH_DEBOUNCE_MS)
+    {
+      continue;
+    }
+
+    bool closed = reading == LOW;
+    if (closed == switchClosed[i])
+    {
+      continue;
+    }
+
+    switchClosed[i] = closed;
+
+    char switchName[10];
+    snprintf(switchName, sizeof(switchName), "Switch %u", i + 1);
+
+    if (closed)
+    {
+      inputClosed(switchName);
+    }
+    else
+    {
+      inputOpened(switchName);
+    }
+  }
+}
 
 void setup()
 {
@@ -48,6 +121,9 @@ void setup()
   // Set microSD Card CS as OUTPUT and set HIGH
   pinMode(SD_CS, OUTPUT);
   digitalWrite(SD_CS, HIGH);
+
+  pinMode(RESET, INPUT_PULLUP);
+  pinMode(PhonePicked, INPUT_PULLUP);
 
   // Initialize SPI bus for microSD Card
   SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
@@ -89,26 +165,20 @@ void loop()
       switch (keypad.key[i].kstate)
       {
       case PRESSED:
-        activeKeys++;
-        Serial.printf("Key %c closed: starting audio\n", keypad.key[i].kchar);
-        audio.stopSong();
-        audio.connecttoFS(SD, AUDIO_FILE);
+      {
+        char keyName[7];
+        snprintf(keyName, sizeof(keyName), "Key %c", keypad.key[i].kchar);
+        inputClosed(keyName);
         break;
+      }
 
       case RELEASED:
-        if (activeKeys > 0)
-        {
-          activeKeys--;
-        }
-
-        Serial.printf("Key %c open\n", keypad.key[i].kchar);
-
-        if (activeKeys == 0)
-        {
-          Serial.println("All keys open: stopping audio");
-          audio.stopSong();
-        }
+      {
+        char keyName[7];
+        snprintf(keyName, sizeof(keyName), "Key %c", keypad.key[i].kchar);
+        inputOpened(keyName);
         break;
+      }
 
       default:
         break;
@@ -116,7 +186,9 @@ void loop()
     }
   }
 
-  if (activeKeys > 0)
+  readSwitches();
+
+  if (activeInputs > 0)
   {
     audio.loop();
   }
